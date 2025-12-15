8a8fbb43cac35732efdd7d20a79f02442b4bf638
wilfrid51 <wilfrid51bergnaumuab@hotmail.com>
2025-12-15 13:41:38 +0000

    add: dataset

--- sft_dataset.py
+++ sft_dataset.py
@@ -226,8 +226,8 @@ def main():
             sampled_task_ids = pool_result.get('sampled_task_ids', [])
             # print(f"✅ {len(sampled_task_ids)} & {sampled_task_ids[:10]} tasks sampled")
             for task_id in sampled_task_ids:
-                # if task_id < 22858:
-                    # continue
+                if task_id < 22858:
+                    continue
                 # print(task_id)
                 if task_ids[env_name][task_id] == 0:
                     # task_id is currently empty, so we can add it to the list
