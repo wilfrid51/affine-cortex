3e1cb7f294fc5908187cfda9f18795ddf05356b6
Tony Dai <tony@example.com>
2025-12-16 03:08:30 +0000

    fix: origin

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,18 +1,3 @@
-git pull
-
-python3 sft_dataset.py SCI
-python3 sft_dataset.py CDE
-python3 sft_dataset.py LGC
-python3 sft_dataset.py MTH
-python3 sft_dataset.py agentgym:alfworld
-python3 sft_dataset.py agentgym:sciworld
 python3 sft_dataset.py agentgym:textcraft
-python3 sft_dataset.py agentgym:webshop
-python3 sft_dataset.py affine:abd-v2
-python3 sft_dataset.py affine:ded-v2
-
-git add .
-git commit -m "add: dataset"
-git push
-
+python3 sft_dataset.py agentgym:sciworld
 sleep 1200
